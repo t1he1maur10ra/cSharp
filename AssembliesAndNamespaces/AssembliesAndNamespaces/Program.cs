26d958b4186fc6952026b5d7a84f75b9bc2e678a
Jacob Van Silfhout <jv1@g.toiohomai.ac.nz>
2018-02-21 20:34:06 +1300

    adding namespaces

---
+++ AssembliesAndNamespaces/AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace AssembliesAndNamespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string text = "hmmmmmmmmmmmmmmmm?";
+            File.WriteAllText(@"C:\testing\testing.txt", text);//From the system.io namespace
+            Console.ReadLine();
+            
+            WebClient client = new WebClient();//From the system.net namespace
+            string reply = client.DownloadString("https://www.google.com");//From the system.net namespace
+            File.WriteAllText(@"C:\testing\googleHtml.text", reply);//From the system.io namespace
+
+            Console.WriteLine(reply);
+            Console.ReadLine();
+
+        }
+    }
+}
